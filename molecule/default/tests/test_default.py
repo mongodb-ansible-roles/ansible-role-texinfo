8e7c78e77ec27d1c4b85ba862ab4a30cf9f3a5d4
John Chen <johnchen456@gmail.com>
2020-01-22 14:29:26 -0500

    test: Added tests for RedHat8

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,19 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_makeinfo_in_path(host):
+    assert host.exists("makeinfo")
+    cmd = host.run("makeinfo --version")
+    assert cmd.stdout == """texi2any (GNU texinfo) 6.7
+
+Copyright (C) 2017 Free Software Foundation, Inc.
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+"""
